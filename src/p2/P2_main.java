55e95e2b93a40e7945c2c101d0be24f93c7e79ce
mhundt2 <marcjack.hundt@gmail.com>
2025-10-30 16:38:33 +0100

    p2,p3

---
+++ src/p2/P2_main.java
@@ -0,0 +1,12 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+        int i = 24;
+        boolean b = false;
+
+        if (i > 10) {
+            b = true;
+        }
+        System.out.println("der wert von b ist:  " + b);
+    }}
---
+++ src/p3/P3_main.java
@@ -0,0 +1,13 @@
+package p3;
+
+public class P3_main {
+    public static void main(String[] args) {
+        int i = 45;
+        int j = 27;
+        int k = 14;
+        boolean ausdruck1 = i < j;
+        boolean ausdruck2 = i < k;
+        boolean ausdruck3 = j < k;
+
+    }
+}
